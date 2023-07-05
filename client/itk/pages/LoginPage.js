529f2ac872b9c879c4df77f7be29c723c4afe1ae
Caleb Intal <fintal@ucsc.edu>
2023-07-05 14:35:54 -0700

    login, sign up, pickle image in asset

---
+++ client/itk/pages/LoginPage.js
@@ -0,0 +1,210 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, SafeAreaView, Image, ImageBackground, TextInput, Button, TouchableOpacity, Alert} from "react-native";
+import { useNavigation } from '@react-navigation/native';
+const LoginPage = () => {
+	const navigation = useNavigation();
+  const [usernme, onChangeUsrn] = React.useState('');
+  const [userpswd, onChangePswd] = React.useState('');
+
+	return (
+		<SafeAreaView style = {{flex: 16, backgroundColor: "#176089"}}>
+			<Image
+			  style={styles.ball1}
+				source={require("../assets/blurpickle.png")}
+			></Image>
+
+			<Image
+			  style={styles.ball2}
+				source={require("../assets/blurpickle.png")}
+			></Image>
+
+			<Image
+			  style={styles.ball3}
+				source={require("../assets/blurpickle.png")}
+			></Image>
+
+			<Image
+			  style={styles.ball4}
+				source={require("../assets/blurpickle.png")}
+			></Image>
+
+			<Image
+			  style={styles.ball5}
+				source={require("../assets/blurpickle.png")}
+			></Image>
+
+			<View style = {styles.login}>
+				<Text style = {{fontSize: 30, color: 'white'}}>Login</Text>
+			</View>
+
+			<TextInput style = {textboxStyle.user}
+				placeholder = 'Username'
+				placeholderTextColor={'maroon'}
+				autoCapitalize="none"
+				onChangeText={onChangeUsrn}
+				value={usernme}
+			></TextInput>
+
+			<TextInput style = {textboxStyle.pswd}
+				placeholder = 'Password'
+				placeholderTextColor={'maroon'}
+				autoCapitalize="none"
+				secureTextEntry={true}
+				onChangeText={onChangePswd}
+				value={userpswd}
+			></TextInput>
+
+			<TouchableOpacity style={buttonStyle.logbut}
+				onPress={()=>Alert.alert("Login button pressed")}>
+				<Text>LOGIN</Text>
+			</TouchableOpacity>
+
+			<View style = {styles.signup}>
+				<Text style = {{fontSize: 18, color: 'white'}}>if you dont have an account,</Text>
+			</View>
+
+			<TouchableOpacity style={buttonStyle.signupbut}
+				onPress={() => navigation.navigate('Sign Up')}>
+				<Text style={{color: 'white', fontSize: 17, textDecorationLine: 'underline'}}>Sign Up!</Text>
+			</TouchableOpacity>
+
+		</SafeAreaView>
+	);
+};
+
+
+const buttonStyle = StyleSheet.create({
+	logbut: {
+		borderRadius: 10,
+		height: 38,
+		width: 78,
+		backgroundColor: 'white',
+		position: 'absolute',
+		top: 530,
+		bottom: 0,
+		left: 54,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+	signupbut: {
+		height: 30,
+		width: 62,
+		// backgroundColor: 'blue',
+		position: 'absolute',
+		top: 585,
+		bottom: 0,
+		left: 270,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+})
+
+const textboxStyle = StyleSheet.create({
+	user: {
+		backgroundColor: 'white',
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+		width: 250,
+		position: 'absolute',
+		top: 390,
+		bottom: 0,
+		left: 40,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+	pswd: {
+		backgroundColor: 'white',
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+		width: 250,
+		position: 'absolute',
+		top: 450,
+		bottom: 0,
+		left: 40,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+});
+
+const styles = StyleSheet.create({
+	signup: {
+		// backgroundColor: 'green',
+		height: 20,
+		width: 230,
+		position: 'absolute',
+		top: 590,
+		bottom: 0,
+		left: 40,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+	login: {
+		position: 'absolute',
+		top: 180,
+		bottom: 320,
+		left: 0,
+		right: 200,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+  ball1: {
+		justifyContent: 'center',
+		right: 160,
+		top: -170,
+		height: '100%',
+		width: '100%',
+		resizeMode: 'contain',
+  },
+	ball2: {
+		justifyContent: 'center',
+		right: -200,
+		top: -800,
+		height: '90%',
+		width: '90%',
+		resizeMode: 'contain',
+  },
+	ball3: {
+		justifyContent: 'center',
+		right: 0,
+		top: -1250,
+		height: '70%',
+		width: '70%',
+		resizeMode: 'contain',
+  },
+	ball4: {
+		justifyContent: 'center',
+		right: -180,
+		top: -1550,
+		height: '90%',
+		width: '90%',
+		resizeMode: 'contain',
+  },
+	ball5: {
+		justifyContent: 'center',
+		position: 'absolute',
+		right: 160,
+		top: 500,
+		height: '100%',
+		width: '100%',
+		resizeMode: 'contain',
+  },
+});
+
+export default LoginPage;
---
+++ client/itk/pages/SignUpPage.js
@@ -0,0 +1,161 @@
+import * as React from "react";
+import { View, Text, StyleSheet, SafeAreaView, Image, ImageBackground, TextInput, Button, TouchableOpacity, Alert} from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
+const SignUpPage = () => {
+	const navigation = useNavigation();
+  const [fname, onChangeFname] = React.useState('');
+  const [lname, onChangeLname] = React.useState('');
+  const [usrnm, onChangeUsrn] = React.useState('');
+  const [usrpwd, onChangePswd] = React.useState('');
+
+	const submitForm = () => {fetch('http://localhost:5000/auth/register', {
+		method: 'POST',
+		body: JSON.stringify({
+			fName: fname,
+			lName: lname,
+			username: usrnm,
+			password: usrpwd
+		})
+		.then(res => {console.log(res.json())})
+	})}
+
+	return (
+		<SafeAreaView style = {{flex: 16, backgroundColor: '#176089', alignContent: 'space-between'}}>
+			<View style={{bottom: 100}}>
+				<TextInput style = {textboxStyle.fname}
+					placeholder = 'First Name'
+					placeholderTextColor={'maroon'}
+					autoCapitalize='words'
+					onChangeText={onChangeFname}
+					value={fname}
+				></TextInput>
+
+				<TextInput style = {textboxStyle.lname}
+					placeholder = 'Last Name'
+					placeholderTextColor={'maroon'}
+					autoCapitalize='words'
+					onChangeText={onChangeLname}
+					value={lname}
+				></TextInput>
+
+				<TextInput style = {textboxStyle.user}
+					placeholder = 'Username'
+					placeholderTextColor={'maroon'}
+					autoCapitalize="none"
+					onChangeText={onChangeUsrn}
+					value={usrnm}
+				></TextInput>
+
+				<TextInput style = {textboxStyle.pswd}
+					placeholder = 'Password'
+					placeholderTextColor={'maroon'}
+					autoCapitalize="none"
+					secureTextEntry={true}
+					onChangeText={onChangePswd}
+					value={usrpwd}
+				></TextInput>
+			</View>
+
+			<TouchableOpacity style={buttonStyle.picklebut}
+				onPress={()=>submitForm}>
+				<Text>Get to Pickling</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+				onPress={() => navigation.navigate('Login')}>
+				<Text style={{color: 'white', fontSize: 17, textDecorationLine: 'underline', left: 20}}>Sign Out :(</Text>
+			</TouchableOpacity>
+		</SafeAreaView>
+	);
+};
+
+const buttonStyle = StyleSheet.create({
+	picklebut: {
+		borderRadius: 10,
+		height: 38,
+		width: 78,
+		backgroundColor: 'white',
+		position: 'absolute',
+		top: 480,
+		bottom: 0,
+		left: 150,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+})
+
+const textboxStyle = StyleSheet.create({
+	fname: {
+		backgroundColor: 'white',
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+		width: 250,
+		position: 'absolute',
+		top: 270,
+		bottom: 0,
+		left: 40,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+	lname: {
+		backgroundColor: 'white',
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+		width: 250,
+		position: 'absolute',
+		top: 330,
+		bottom: 0,
+		left: 40,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+	user: {
+		backgroundColor: 'white',
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+		width: 250,
+		position: 'absolute',
+		top: 390,
+		bottom: 0,
+		left: 40,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+	pswd: {
+		backgroundColor: 'white',
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 10,
+		width: 250,
+		position: 'absolute',
+		top: 450,
+		bottom: 0,
+		left: 40,
+		right: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+});
+
+export default SignUpPage;
