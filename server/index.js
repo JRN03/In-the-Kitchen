e45824fb17f07dba50449263c8c13e263e348d23
nick <nbarsirh@ucsc.edu>
2023-07-05 16:21:47 -0700

    fixing port issue

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import auth from './routes/auth.js';
-import courts from './routes/courts.js';
+import auth from "./routes/auth.js";
+import courts from "./routes/courts.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -16,19 +16,16 @@ app.use(bodyParser.json());
 
 dotenv.config();
 
-mongoose.connect(
-    process.env.DB_CONNECT,
-    {useNewUrlParser:true}
-);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true });
 
-app.get('/', (req,res) => {
-    res.status(200).send("Home Route of Server");
+app.get("/", (req, res) => {
+  res.status(200).send("Home Route of Server");
 });
 
-app.use('/auth',auth);
-app.use('/courts',courts);
+app.use("/auth", auth);
+app.use("/courts", courts);
 
 const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 server.keepAliveTimeout = 65000;
