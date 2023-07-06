4eaa14eb11f3f46d5ca2efe2fc565125bf08fa41
nick <nbarsirh@ucsc.edu>
2023-07-06 10:53:47 -0700

    components for profile page

--- client/itk/components/BioText.js
+++ client/itk/components/BioText.js
@@ -1,23 +1,69 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, Button, TextInput } from "react-native";
 
-const text = "Bio is my text";
 const BioText = () => {
+  const [bioText, setBioText] = useState("Write your bio here...");
+  const [editButton, setEditButton] = useState(true);
+
+  let text = <Text style={styles.textBox}>{bioText}</Text>;
+
+  const bioEditHandler = () => {
+    setEditButton(!editButton);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textBox}>{text}</Text>
+      {editButton ? (
+        <Text style={styles.textBox}>{bioText}</Text>
+      ) : (
+        <TextInput
+          style={styles.writeableTextBox}
+          placeholder="Please type your bio here..."
+          maxLength={90}
+          numberOfLines={4}
+          multiline
+          onChangeText={(text) => setBioText(text)}
+        ></TextInput>
+      )}
+      {editButton ? (
+        <Button
+          title="Edit"
+          onPress={bioEditHandler}
+          style={{ margin: 10 }}
+        ></Button>
+      ) : (
+        <Button
+          title="Done"
+          onPress={bioEditHandler}
+          style={{ margin: 10 }}
+        ></Button>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  // adjust flex of page to format border line of the header
   container: {
     flex: 1,
     backgroundColor: "#176089",
     alignItems: "center",
+    maxHeight: 135,
   },
   textBox: {
+    color: "black",
+    backgroundColor: "white",
+    borderColor: "white",
+    fontSize: 16,
+    overflow: "hidden",
+    height: 100,
+    width: 250,
+    borderRadius: 15,
+    marginTop: 15,
+    padding: 10,
+    textAlign: "center",
+    textAlignVertical: "center",
+  },
+  writeableTextBox: {
     color: "black",
     backgroundColor: "white",
     borderColor: "white",
---
+++ client/itk/components/ImagePicker.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export default ImagePickerExample = () => {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+    console.log(image);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={
+          !image ? require("../assets/TempProfilePic.jpeg") : { uri: image }
+        }
+        style={{ width: 150, height: 150, borderRadius: 150 / 2 }}
+      />
+      <Button title="Edit" onPress={pickImage} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    maxHeight: 200,
+    minHeight: 180,
+    alignItems: "center",
+  },
+});
