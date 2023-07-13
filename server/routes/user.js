bb917875c0e64d0c9c09b32edd96f2f85ddb0c98
JRN03 <jonathan.nguyen2021@gmail.com>
2023-07-13 10:44:25 -0700

    Update server side routes responses and json data limit

--- server/routes/user.js
+++ server/routes/user.js
@@ -13,18 +13,15 @@ const __dirname = path.dirname(__filename);
 router.get("/", verify, async (req, res) => {
   const id = req.id;
   const user = await User.findOne({ _id: id });
-  res.status(200).send(user);
+  if (user) return res.status(200).send({fName: user.fName, lName: user.lName, username: user.username, friends: user.friends, bio: user.bio, image: user.image});
+  return res.status(404).send({message:"ID not Found"});
 });
 
 router.get("/pfp", async (req, res) => {
   const id = req.body.id;
   const user = await User.findOne({ _id: id });
-  res.status(200).sendFile(__dirname + `/resources/${user.image}`);
-});
-
-router.put("/bio", verify, async (req, res) => {
-  const id = req.id;
-  const user = await User.findOne({ _id: id });
+  if (user) return res.status(200).sendFile(__dirname + `/resources/${user.image}`);
+  return res.status(404).send({message:"ID not Found"});
 });
 
 router.put("/pfp", verify, async (req, res) => {
@@ -40,28 +37,44 @@ router.put("/pfp", verify, async (req, res) => {
   const numFiles = files.length;
 
   const uri = req.body.uri;
-  console.log(req.body);
-  console.log(id);
+  const base64Data = uri.split(";base64,").pop(); // Extract base64 data without the prefix
+
+  console.log("req body",req.body);
   // return;
 
   const fileExtension = "jpg"
   const filename = `user_image-${numFiles + 1}.${fileExtension}`;
   const filePath = path.join(resources, filename);
 
-  fs.writeFile(filePath, uri, async (err) => {
+  fs.writeFile(filePath, base64Data, {encoding:'base64'},async (err) => {
     if (err || !User.findOne({ _id: id })) {
       return res.status(500).send({ message: "Failed to update image" });
     } else {
-      await User.updateOne({ _id: id }, { image: filename });
-      console.log(id);
-      return res.status(201).send({ message: "Image Updated" });
+      const user = await User.updateOne({ _id: id }, { image: filename });
+      if (user) return res.status(201).send({ message: "Image Updated" });
     }
   });
+  return res.status(404).send({message:"ID not Found"});
+});
+
+router.put("/bio", verify, async (req, res) => {
+  const id = req.id;
+  const user = await User.findOne({ _id: id });
+  if (!user) return res.status(404).send({message:"ID not Found"});
+  const success = await User.updateOne({_id:id},{bio:req.body.body});
+  if (success) return res.status(200).send({message: "Bio Updated"});
+  return res.status(500).send({message:"Internal Server Error"});
 });
 
 router.put("/friend", verify, async (req, res) => {
   const id = req.id;
   const user = await User.findOne({ _id: id });
+  if (!user) return res.status(404).send({message:"ID not Found"});
+  const friend = await User.findOne({username:req.body.username});
+  if (!friend) return res.status(404).send({message:"User not Found"});
+  const success = await User.updateOne({_id:id},{$push: {friends: req.body.username}});
+  if (success) return res.status(200).send({message: "Friend Added"});
+  return res.status(500).send({message:"Internal Server Error"});
 });
 
 export default router;
