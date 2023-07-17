00b61c83582c75e98898a769200be8a5761a692b
nick <nbarsirh@ucsc.edu>
2023-07-16 22:52:58 -0700

    pushing for jonathan

--- client/itk/components/Carousel.js
+++ client/itk/components/Carousel.js
@@ -1,54 +1,65 @@
-import React, {useRef, useState, Component,useEffect} from 'react';
-import { Dimensions, SafeAreaView, Text, View,StyleSheet, ScrollView,Image,useWindowDimensions, } from 'react-native';
+import React, { useRef, useState, Component, useEffect } from "react";
+import {
+  Dimensions,
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Image,
+  useWindowDimensions,
+} from "react-native";
 // import  Animated  from 'react-native-reanimated';
-import Carousel, {ParallaxImage} from 'react-native-new-snap-carousel';
+import Carousel, { ParallaxImage } from "react-native-new-snap-carousel";
 
 const DATA = [
-    {image:require("../assets/shaq.jpeg"),title:"SHAQ"},
-    {image:require("../assets/shaq.jpeg"),title:"SHAQ"},
-    {image:require("../assets/shaq.jpeg"),title:"SHAQ"},
-]
-const MyCarousel = props => {
-    const {height, width, scale, fontScale} = useWindowDimensions();
+  { image: require("../assets/shaq.jpeg"), title: "SHAQ" },
+  { image: require("../assets/shaq.jpeg"), title: "SHAQ" },
+  { image: require("../assets/shaq.jpeg"), title: "SHAQ" },
+];
+const MyCarousel = (props) => {
+  const { height, width, scale, fontScale } = useWindowDimensions();
 
-    const [entries, setEntries] = useState([]);
-    const carouselRef = useRef(null);
+  const [entries, setEntries] = useState([]);
+  const carouselRef = useRef(null);
 
-    useEffect(() => {
-        setEntries(DATA);
-    }, [props]);
+  useEffect(() => {
+    setEntries(DATA);
+  }, [props]);
 
-    const renderItem = ({item, index}) => {
-        return (
-            <View style={styles.slide}>
-                <Image style = {styles.image} source={item.image}></Image>
-            </View>
-        );
-    }
+  const renderItem = ({ item, index }) => {
+    return (
+      <View style={styles.slide}>
+        <Image style={styles.image} source={item.image}></Image>
+      </View>
+    );
+  };
 
-        return (
-            <View style = {[styles.container,{width:width}]}>
-                <Carousel
-                    ref={carouselRef}
-                    data={DATA}
-                    renderItem={renderItem}
-                    sliderWidth={300}
-                    itemWidth={300}
-                    activeSlideAlignment = "center"
-                    layout ={'default'}
-                    contentContainerCustomStyle = {styles.container}
-                />
-            </View>
-
-        );
-
-
-}
+  return (
+    <Carousel
+      ref={carouselRef}
+      data={DATA}
+      renderItem={renderItem}
+      sliderWidth={width}
+      itemWidth={210}
+      activeSlideAlignment="center"
+      layout={"default"}
+      contentContainerCustomStyle={styles.container}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    slide:{maxWidth:"70%", alignContent:"center"},
-    title:{fontSize:16},
-    image:{height:"90%",aspectRatio:1, borderRadius:20},
-    container:{justifyContent: "center",height:200, }
-})
-export default MyCarousel
+  //   slide: { maxWidth: "100%", alignContent: "center" },
+  title: { fontSize: 16 },
+  image: { height: "100%", aspectRatio: 1, borderRadius: 20 },
+  container: {
+    height: 200,
+    // width: "100%",
+    borderColor: "blue",
+    borderWidth: 2,
+    alignContent: "center",
+    // justifyContent: "center",
+  },
+});
+export default MyCarousel;
--- client/itk/pages/ParkView.js
+++ client/itk/pages/ParkView.js
@@ -1,16 +1,49 @@
-import { View, StyleSheet, Text, ScrollView, SafeAreaView, Alert, processColor, TouchableOpacity } from "react-native";
-import MapView, {Marker, PROVIDER_GOOGLE} from "react-native-maps";
-import {React, useState, useEffect, useRef,Component} from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  Alert,
+  processColor,
+  TouchableOpacity,
+} from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { React, useState, useEffect, useRef, Component } from "react";
 import AppHeader from "../components/AppHeader";
 import Navbar from "../components/Navbar";
-import {PageStyles} from "../assets/Styles";
+import { PageStyles } from "../assets/Styles";
 import light from "../assets/themes/light.js";
 import ParkCard from "../components/ParkCard";
 import Review from "../components/Review";
 import MyCarousel from "../components/Carousel";
 
 import {
-    useFonts,
+  useFonts,
+  RobotoSlab_100Thin,
+  RobotoSlab_200ExtraLight,
+  RobotoSlab_300Light,
+  RobotoSlab_400Regular,
+  RobotoSlab_500Medium,
+  RobotoSlab_600SemiBold,
+  RobotoSlab_700Bold,
+  RobotoSlab_800ExtraBold,
+  RobotoSlab_900Black,
+} from "@expo-google-fonts/roboto-slab";
+
+const meetingTimes = ["Friday: 9am-12pm", "Tuesday: 9am-12pm"];
+
+// export default function ParkView({navigation,route}) {
+export default function ParkView({ navigation, route }) {
+  const token = route.params.token;
+
+  var { props } = route.params;
+  console.log("ParkView", props);
+  const [lat, setLat] = useState(props.lat);
+  const [lon, setLon] = useState(props.lon);
+  const [mapLatDelta, setMapLatDelta] = useState(0.1);
+  const [mapLonDelta, setMapLonDelta] = useState(0.12050628662110796);
+  let [fontsLoaded] = useFonts({
     RobotoSlab_100Thin,
     RobotoSlab_200ExtraLight,
     RobotoSlab_300Light,
@@ -20,71 +53,59 @@ import {
     RobotoSlab_700Bold,
     RobotoSlab_800ExtraBold,
     RobotoSlab_900Black,
-} from '@expo-google-fonts/roboto-slab';
+  });
 
-const meetingTimes = ['Friday: 9am-12pm', 'Tuesday: 9am-12pm'];
+  if (!fontsLoaded) {
+    return null;
+  }
 
-// export default function ParkView({navigation,route}) {
-export default function ParkView({navigation,route}) {
-    const token = route.params.token;
-   
-    var {props} = route.params;
-    console.log("ParkView",props);
-    const [lat,setLat] =  useState(props.lat);
-    const [lon,setLon] =  useState(props.lon);
-    const [mapLatDelta,setMapLatDelta] = useState(.1);
-    const [mapLonDelta,setMapLonDelta] = useState(0.12050628662110796);
-    let [fontsLoaded] = useFonts({
-        RobotoSlab_100Thin,
-        RobotoSlab_200ExtraLight,
-        RobotoSlab_300Light,
-        RobotoSlab_400Regular,
-        RobotoSlab_500Medium,
-        RobotoSlab_600SemiBold,
-        RobotoSlab_700Bold,
-        RobotoSlab_800ExtraBold,
-        RobotoSlab_900Black,
-    });
+  // we need some type of review parser.
 
-    if (!fontsLoaded) {
-      return null;
-    }
-   
-
-
-    // we need some type of review parser.
-
-    return(
-        <SafeAreaView style={PageStyles.main}>
-            <AppHeader/>
-            <View style={PageStyles.contentWrap}>
-                <View style = {styles.textContainer}>
-                    <Text style = {styles.introText}>{props.name}</Text>
-                </View>
-                <Review 
-                    ratings = {props.rating}
-                    placesID = {props.placesID}
-                />
-                <View style = {{ width: "100%"}}>
-                  <MyCarousel/>
-                </View>
-                <ParkCard
-                    name = {props.name}
-                    location = {props.location}
-                    meetTimes = {meetingTimes}
-                    lat = {lat}
-                    lon = {lon}
-                />
-                <Navbar route={route} token={token}/>
-                <Text onPress = {()=>{navigation.navigate('Courts',{token:token})}} style = {{fontSize: 15, marginTop:10,textAlign:'left',color:'white', textDecorationLine: 'underline',}}>Back to Courts</Text>
-
-            </View>
-        </SafeAreaView>
-
-    )
+  return (
+    <SafeAreaView style={PageStyles.main}>
+      <AppHeader />
+      <View style={styles.contentWrap}>
+        <View style={styles.textContainer}>
+          <Text style={styles.introText}>{props.name}</Text>
+        </View>
+        <Review ratings={props.rating} placesID={props.placesID} />
+        <View
+          style={{
+            width: "100%",
+            // borderColor: "red",
+            // borderWidth: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <MyCarousel />
+        </View>
+        <ParkCard
+          name={props.name}
+          location={props.location}
+          meetTimes={meetingTimes}
+          lat={lat}
+          lon={lon}
+        />
+        <Navbar route={route} token={token} />
+        <Text
+          onPress={() => {
+            navigation.navigate("Courts", { token: token });
+          }}
+          style={{
+            fontSize: 15,
+            marginTop: 10,
+            textAlign: "left",
+            color: "white",
+            textDecorationLine: "underline",
+          }}
+        >
+          Back to Courts
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   main: {
@@ -92,43 +113,43 @@ const styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
     backgroundColor: light.primary,
-    position: "absolute"
+    position: "absolute",
   },
-  introText:{
-    color:"white",
-    fontFamily:"RobotoSlab_400Regular",
+  introText: {
+    color: "white",
+    fontFamily: "RobotoSlab_400Regular",
     fontSize: 28,
-    fontWeight:'bold',
-    textAlign: 'left',
+    fontWeight: "bold",
+    textAlign: "left",
   },
-  textContainer:{
-    marginTop: 10
-  },
-  contentWrap:{
-    width: "90%",
-    flex: 1,
-    position: "absolute"
+  textContainer: {
+    marginTop: 10,
   },
   map: {
     height: "25%",
     width: "100%",
     borderRadius: 10,
     position: "relative",
-    marginTop:20,
-    zIndex: 0
+    marginTop: 20,
+    zIndex: 0,
   },
-  nearbyContainer:{
+  nearbyContainer: {
     width: "100%",
     flexGrow: 0,
     maxHeight: "32%",
   },
+  contentWrap: {
+    width: "90%",
+    flex: 1,
+    position: "relative",
+  },
   button: {
     // flex: 1,
-    alignItems:"center",
+    alignItems: "center",
     justifyContent: "center",
     borderRadius: 10,
     height: 30,
     backgroundColor: "#176089",
-    paddingTop:5
-},
+    paddingTop: 5,
+  },
 });
