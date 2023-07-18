c51cbe556eec97d740442b9341bdd715fa533553
nick <nbarsirh@ucsc.edu>
2023-07-17 23:24:05 -0700

    created chatMessage component

---
+++ client/itk/components/ChatComponent.js
@@ -0,0 +1,86 @@
+import * as React from "react";
+import {
+  Pressable,
+  Image,
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { PageStyles } from "../assets/Styles";
+import light from "../assets/themes/light";
+import { ListItem, Avatar } from "@rneui/themed";
+
+import {
+  useFonts,
+  RobotoSlab_100Thin,
+  RobotoSlab_200ExtraLight,
+  RobotoSlab_300Light,
+  RobotoSlab_400Regular,
+  RobotoSlab_500Medium,
+  RobotoSlab_600SemiBold,
+  RobotoSlab_700Bold,
+  RobotoSlab_800ExtraBold,
+  RobotoSlab_900Black,
+} from "@expo-google-fonts/roboto-slab";
+
+const toMessenger = () => {
+  console.log("To the messenger");
+};
+
+export default ChatComponent = ({ route }) => {
+  let [fontsLoaded] = useFonts({
+    RobotoSlab_100Thin,
+    RobotoSlab_200ExtraLight,
+    RobotoSlab_300Light,
+    RobotoSlab_400Regular,
+    RobotoSlab_500Medium,
+    RobotoSlab_600SemiBold,
+    RobotoSlab_700Bold,
+    RobotoSlab_800ExtraBold,
+    RobotoSlab_900Black,
+  });
+  return (
+    <TouchableOpacity onPress={toMessenger} style={styles.chatContainer}>
+      <Image
+        source={require("../assets/TempProfilePic.jpeg")}
+        style={styles.image}
+      ></Image>
+      <Text style={styles.chat}>Name Here</Text>
+      <ListItem.Chevron color="white" size={40} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  chatContainer: {
+    borderColor: "white",
+    borderWidth: 1,
+    flex: 1,
+    flexDirection: "row",
+    minHeight: 60,
+    maxHeight: 70,
+    backgroundColor: light.secondary,
+    marginTop: 10,
+    shadowColor: light.shadow,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: "25%",
+    shadowRadius: 4,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  chat: {
+    fontFamily: "RobotoSlab_400Regular",
+    color: "white",
+    // borderColor: "red",
+    // borderWidth: 1,
+    width: "60%",
+  },
+  image: {
+    maxHeight: 50,
+    maxWidth: 50,
+    marginRight: 25,
+    marginLeft: 10,
+    borderRadius: 25,
+  },
+});
--- client/itk/pages/Messages.js
+++ client/itk/pages/Messages.js
@@ -1,16 +1,54 @@
 import * as React from "react";
-import { SafeAreaView,View } from "react-native";
+import { Pressable, SafeAreaView, View, Text, StyleSheet } from "react-native";
 import Navbar from "../components/Navbar";
 import { PageStyles } from "../assets/Styles";
 import AppHeader from "../components/AppHeader";
+import light from "../assets/themes/light";
+import { useNavigation } from "@react-navigation/native";
+import ChatComponent from "../components/ChatComponent";
 
-export default function Messages({route}){
-    return (
-        <SafeAreaView style={PageStyles.main}>
-            <AppHeader route={route}/>
-            <View style={PageStyles.contentWrap}>
-                <Navbar route={route}/>
-            </View>
-        </SafeAreaView>
-    )
-}
+import {
+  useFonts,
+  RobotoSlab_100Thin,
+  RobotoSlab_200ExtraLight,
+  RobotoSlab_300Light,
+  RobotoSlab_400Regular,
+  RobotoSlab_500Medium,
+  RobotoSlab_600SemiBold,
+  RobotoSlab_700Bold,
+  RobotoSlab_800ExtraBold,
+  RobotoSlab_900Black,
+} from "@expo-google-fonts/roboto-slab";
+
+const newMessage = () => {
+  console.log("Create new message");
+};
+
+const toMessenger = () => {
+  console.log("To the messenger");
+};
+
+export default Messages = ({ route }) => {
+  let [fontsLoaded] = useFonts({
+    RobotoSlab_100Thin,
+    RobotoSlab_200ExtraLight,
+    RobotoSlab_300Light,
+    RobotoSlab_400Regular,
+    RobotoSlab_500Medium,
+    RobotoSlab_600SemiBold,
+    RobotoSlab_700Bold,
+    RobotoSlab_800ExtraBold,
+    RobotoSlab_900Black,
+  });
+  return (
+    <SafeAreaView style={PageStyles.main}>
+      <AppHeader route={route} action={newMessage} />
+      <View style={PageStyles.contentWrap}>
+        <ChatComponent></ChatComponent>
+        <Navbar route={route} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
