99092dd213b891fb3842cd4e5b4be3a3c030f13f
nick <nbarsirh@ucsc.edu>
2023-07-18 20:15:22 -0700

    cleaned up server code

--- server/message_server.js
+++ server/message_server.js
@@ -1,3 +1,12 @@
+/*
+
+
+    !!!!TO RUN SERVER!!!!
+        npm run dev
+
+
+*/
+
 import express from "express";
 import cors from "cors";
 import http from "http";
@@ -5,10 +14,6 @@ import { Server } from "socket.io";
 const app = express();
 const PORT = 4000;
 
-const CHAT_BOT = "ChatBot";
-
-let chatRoom = "";
-let allUsers = [];
 let chatRooms = [];
 
 app.use(cors());
@@ -24,36 +29,11 @@ const io = new Server(server, {
 io.on("connection", async (socket) => {
   console.log(`User connected ${socket.id}`);
   socket.on("createRoom", (data) => {
-    console.log(data);
     const { uname, room } = data;
-    console.log(uname + " " + room);
     socket.join(room);
     chatRooms.unshift({ id: uname, room, messages: [] });
     socket.emit("roomsList", chatRooms);
   });
-  // socket.on("join_room", (data) => {
-  //   console.log(data);
-  //   const { uname, room } = data; // Data sent from client when join_room event emitted
-  //   console.log("room =", room);
-  //   socket.join(room);
-  //   let __createdtime__ = Date.now(); // Current timestamp
-  //   socket.to(room).emit("receive_message", {
-  //     message: `${uname} has joined the chat room`,
-  //     username: CHAT_BOT,
-  //     __createdtime__,
-  //   });
-  //   chatRoom = room;
-  //   allUsers.push({ id: socket.id, uname, room });
-  //   const chatRoomUsers = allUsers.filter((user) => user.room === room);
-  //   socket.to(room).emit("chatroom_users", chatRoomUsers);
-  //   console.log("all users: ", allUsers);
-  // });
-  // socket.on("findRoom", (id) => {
-  //   let result = chatRooms.filter((room) => room.id == id);
-  //   // console.log(chatRooms);
-  //   socket.emit("foundRoom", result[0].messages);
-  //   // console.log("Messages Form", result[0].messages);
-  // });
   socket.on("disconnect", () => {
     socket.disconnect();
     console.log("user disconnected");
