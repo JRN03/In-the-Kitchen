0cb4942f4d750ebc757a43acc69ad4a849ffc346
JRN03 <jonathan.nguyen2021@gmail.com>
2023-07-18 18:50:20 -0700

    Updated to match dev

--- server/models/court.js
+++ server/models/court.js
@@ -9,9 +9,13 @@ const courtsSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    rating: [Number],
+    rating: {
+        type: [Number],
+        default: [0]
+    },
     times: {    // meeting times for events -- type string & will just default to "N/A"
-        type: [{day:String, start:String, end:String}]
+        type: [{day:String, start:String, end:String}],
+        default: []
     },
     placesID: {
         type: String,
@@ -26,7 +30,8 @@ const courtsSchema = new mongoose.Schema({
         required:true
     },
     images: {
-        type: [String]
+        type: [String],
+        default: []
     }
 
 });
